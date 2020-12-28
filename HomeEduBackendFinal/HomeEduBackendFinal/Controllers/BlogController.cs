23a9b577fa99160b75471f32f3c6fb1bb2a99b43
NahidaM <nahidanm@code.edu.az>
2020-12-29 01:04:29 +0400

    Pagination for Index of Blog

--- HomeEduBackendFinal/HomeEduBackendFinal/Controllers/BlogController.cs
+++ HomeEduBackendFinal/HomeEduBackendFinal/Controllers/BlogController.cs
@@ -18,6 +18,24 @@ namespace HomeEduBackendFinal.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Index(int? page)
+        {
+            ViewBag.PageCount = Math.Ceiling((decimal)_db.Blogs.Count() / 3);
+            ViewBag.Page = page;
+            if (page == null)
+            {
+                return View(_db.Blogs.OrderByDescending(p => p.Id).Take(3).ToList());
+            }
+            else
+            {
+                return View(_db.Blogs.OrderByDescending(p => p.Id).Skip(((int)page - 1) * 3).Take(3).ToList());
+            } 
+        }
+        public IActionResult Detail()
+        {
+            return View();
         } 
     }
 }
