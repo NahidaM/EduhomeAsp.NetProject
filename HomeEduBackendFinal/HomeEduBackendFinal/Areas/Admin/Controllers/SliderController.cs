69ff690ef7ac29be26c94c4d88fbb1d56e391610
NahidaM <nahidanm@code.edu.az>
2021-01-02 16:27:31 +0400

    CRUD for Slider

---
+++ HomeEduBackendFinal/HomeEduBackendFinal/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,164 @@
+﻿using HomeEduBackendFinal.DAL;
+using HomeEduBackendFinal.Extentions;
+using HomeEduBackendFinal.Helpers;
+using HomeEduBackendFinal.Models;
+using HomeEduBackendFinal.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeEduBackendFinal.Areas.Admin.Controllers
+{
+    [Area("Admin")] 
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _db;
+        private readonly IHostingEnvironment _env;
+        public SliderController(AppDbContext db, IHostingEnvironment env) 
+        {
+            _db = db;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Sliders.ToList());
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null) return NotFound();
+            Slider slider = await _db.Sliders.FindAsync(id);
+            if (slider == null) return NotFound();
+            return View(slider);
+        }
+
+        public IActionResult Create()
+        {
+            if (_db.Sliders.Count() >= 5)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(Slider slider)
+        {
+
+            if (ModelState["Photo"].ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
+            {
+                return View();
+            }
+
+            //if (!slider.Photo.IsImage())
+            //{
+            //    ModelState.AddModelError("Photo", "Zehmet olmasa shekil formati sechin");
+            //    return View();
+            //}
+
+            //if (slider.Photo.MaxLength(2000))
+            //{
+            //    ModelState.AddModelError("Photo", "Shekilin olchusu max 200kb ola biler");
+            //    return View();
+            //}
+
+            if (_db.Sliders.Count() >= 5)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            string path = Path.Combine("img", "slider"); 
+            //string fileName = await slider.Photo.SaveImg(_env.WebRootPath, path);
+
+            Slider newslider = new Slider();
+            newslider.Description = slider.Description;
+            newslider.Title = slider.Title;
+            //newslider.Image = fileName; 
+
+            await _db.Sliders.AddAsync(newslider);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            Slider slider = await _db.Sliders.FindAsync(id);
+            if (slider == null) return NotFound();
+            return View(slider);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeletePost(int? id)
+        {
+            if (id == null) return NotFound();
+            Slider slider = await _db.Sliders.FindAsync(id);
+            if (slider == null) return NotFound();
+            string path = Path.Combine("img", "slider");
+            Helper.DeleteImage(_env.WebRootPath, path, slider.Image);
+
+            _db.Sliders.Remove(slider);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null) return NotFound();
+            Slider slider = await _db.Sliders.FindAsync(id);
+            if (slider == null) return NotFound();
+            return View(slider); 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, SliderVM createSliderVM)
+        {
+            if (id == null) return NotFound();
+            Slider dbSlider = await _db.Sliders.FindAsync(id);
+            if (dbSlider == null) return NotFound();
+            if (createSliderVM.Photo != null)
+            {
+                if (ModelState["Photo"].ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
+                {
+                    return View();
+                }
+
+                //if (!createSliderVM.Photo.IsImage())
+                //{
+                //    ModelState.AddModelError("Photo", "Zehmet olmasa shekil formati sechin");
+                //    return View();
+                //}
+
+                //if (createSliderVM.Photo.MaxLength(200))
+                //{
+                //    ModelState.AddModelError("Photo", "Shekilin olchusu max 200kb ola biler");
+                //    return View();
+                //} 
+
+
+                string path = Path.Combine("img", "slider");
+                Helper.DeleteImage(_env.WebRootPath, path, dbSlider.Image);
+
+
+                //string fileName = await createSliderVM.Photo.SaveImg(_env.WebRootPath, path);
+                //dbSlider.Image = fileName; 
+
+            }
+            dbSlider.Description = createSliderVM.Description;
+            dbSlider.Title = createSliderVM.Title;
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
